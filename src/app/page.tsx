6d75a15f2d732f794da4fe3e36e039efbfc91c8e
Crucialjun <nich.otieno@gmail.com>
2023-08-07 23:11:05 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Image from 'next/image'
+import { useState } from 'react'
+
+
 
 export default function Home() {
-  function ListItems(){
-    const ints = [1,2,3,4,5,6,7,8,9,10]
+  function ListItems({ints,addValue} : {ints: number[], addValue: () => void}){
     return (
       <>
+      <button onClick={addValue}>Add Item</button>
      {
       ints.map(id =>{
         return(
-          <li>{id}</li>
+          <li key={id} >{id}</li>
         )
       })
      }
@@ -16,9 +21,15 @@ export default function Home() {
       
     )
   }
+
+  const [ints, setInts] = useState([1,2,3,4,5,6,7,8,9,10])  
+  function addValue(){
+    const newValue = Math.max(...ints) + 1
+    setInts([...ints, newValue])
+  }
   return (
     <ul>
-      <ListItems/>
+      <ListItems ints = {ints} addValue={addValue}/>
     </ul>
   )
 }
